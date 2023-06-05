28403e2498ef4a12ca6e0669d7d42a55a30bf311
MrKeke <alexgraf0701@gmail.com>
2023-06-05 16:42:21 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// https://www.figma.com/file/5Up611L8QTVThfzfi9fwI8/%D0%9A%D1%83%D0%B4%D0%B0-%D0%9F%D0%B8%D1%86%D1%86%D0%B0-(Copy)-(Copy)?type=design&t=jOMbmStDRmNqJjxE-6
+reportWebVitals();
