6d5e9b176534bda078db8bca8a50e4605c993253
Lo0lode <arthassas@mail.ru>
2023-06-07 22:10:31 +0300

    Create Links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,13 @@
 
 import { UploadOutlined, UserOutlined, VideoCameraOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Layout, Menu, Breadcrumb, Button, Image } from 'antd';
+import pizzaIcon from '../Pictures/pizza.jpg'
+import sushiIcon from '../Pictures/sushi.jpg'
+import drinkIcon from '../Pictures/drink.jpg'
+import snackIcon from '../Pictures/snack.jpg'
+import comboIcon from '../Pictures/combo.jpg'
+import dessertIcon from '../Pictures/dessert.jpg'
+import sauceIcon from '../Pictures/sauce.jpg'
 import React from 'react';
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -26,13 +33,14 @@ export function LayoutComponent({children, headerContent}){
         <div className='ml-72 text-xl text-black font-bold'>Hexlet Pizza</div>
         <Button className="bg-orange-400 text-gray-50 mt-4 ml-[750px]"><ShoppingCartOutlined className='text-lg absolute left-1 bottom-1'></ShoppingCartOutlined>Корзина</Button>
       {/* <div className='ml-10'>Войти в аккаунт</div> */}
-        <Button className='bg-orange-400 text-gray-50 mt-4 ml-8'><UserOutlined className='text-lg absolute bottom-[3px] left-[6px]'></UserOutlined></Button>
+        <Button className='bg-orange-400 text-gray-50 mt-4 ml-6'><UserOutlined className='text-lg absolute bottom-[3px] left-[6px]'></UserOutlined></Button>
       </div>
     </Header>
   <Content
     className="site-layout"
     style={{
-      padding: '0 50px'
+      padding: '0 50px',
+      background: "white"
     }}
   >
     <Breadcrumb
@@ -45,10 +53,39 @@ export function LayoutComponent({children, headerContent}){
       style={{
         padding: 24,
         minHeight: 380,
-        background: "white",
+        backgroundColor:'#f9f9f9'
       }}
     >
-      <div>{children}</div>
+      <div className='flex'>
+        <div className='bg-white w-36 ml-52 items-center pl-10 rounded-md'>
+          <img src={pizzaIcon} className='h-16 w-16'/>
+          <p className='ml-2 font-bold'>Пицца</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={sushiIcon} className='h-16 w-16'/>
+          <p className='ml-4 font-bold'>Суши</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={drinkIcon} className='h-16 w-16'/>
+          <p className='ml-1 font-bold'>Напитки</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={snackIcon} className='h-16 w-16'/>
+          <p className='ml-2.5 font-bold'>Снэки</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={comboIcon} className='h-16 w-16'/>
+          <p className='ml-2.5 font-bold'>Комбо</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={dessertIcon} className='h-16 w-16'/>
+          <p className='ml-1 font-bold'>Десерты</p>
+        </div>
+        <div className='bg-white w-36 ml-10 items-center pl-10 rounded-md'>
+          <img src={sauceIcon} className='h-16 w-16'/>
+          <p className='ml-1 font-bold'>Соусы</p>
+        </div>
+      </div>
     </div>
   </Content>
   <Footer
