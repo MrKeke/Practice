4354ebfd88506f40d59857445163338c3ba4821d
DmitriyM <mironowd697@gmail.com>
2023-06-08 23:20:58 +0600

    add footer in main page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Layout } from 'antd';
 import {HeaderComponent} from "./LayoutComponents/Header";
 import {BodyComponent} from "./LayoutComponents/Body";
-import {FooterComponent} from "./LayoutComponents/Footer";
+import Footer from "./LayoutComponents/Footer";
 import MainPage from "./MainPage";
+
 
 export function LayoutComponent(){
 
@@ -13,7 +14,7 @@ export function LayoutComponent(){
       <BodyComponent>
         <MainPage />
       </BodyComponent>
-      <FooterComponent text={"Hexlet Pizza 2018"}/>
+      <Footer trademark="© Copyright 2021 — Hexlet Pizza"></Footer>
     </Layout>
   );
 };
