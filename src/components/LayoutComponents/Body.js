fb55cb3cf294c654bb2d3d5e5191d174e6e941f9
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-06-08 15:33:39 +0300

    add pizza card

--- src/components/LayoutComponents/Body.js
+++ src/components/LayoutComponents/Body.js
@@ -1,67 +1,80 @@
 import React from "react";
-import {NavGen} from "../NavGen";
+import { NavGen } from "../NavGen";
 import pizzaIcon from "../../Pictures/pizza.jpg";
 import sushiIcon from "../../Pictures/sushi.jpg";
 import drinkIcon from "../../Pictures/drink.jpg";
 import snackIcon from "../../Pictures/snack.jpg";
 import comboIcon from "../../Pictures/combo.jpg";
+import pizzaMedia from "../../Pictures/Rectangle 4.svg";
 import dessertIcon from "../../Pictures/dessert.jpg";
 import sauceIcon from "../../Pictures/sauce.jpg";
-import {Layout} from "antd";
-const {Content} = Layout
-
+import { Layout } from "antd";
+import { Product } from "../product/product";
+const { Content } = Layout;
 
 const navLinks = [
-  {
-    name: "Пицца",
-    icon: pizzaIcon
-  },
-  {
-    name: "Суши",
-    icon: sushiIcon
-  },
-  {
-    name: "Напитки",
-    icon: drinkIcon,
-  },
-  {
-    name: "Снэки",
-    icon: snackIcon,
-  },
-  {
-    name: "Комбо",
-    icon: comboIcon,
-  },
-  {
-    name: "Десерты",
-    icon: dessertIcon,
-  },
-  {
-    name: "Соусы",
-    icon: sauceIcon,
-  },
-]
-export const BodyComponent = ({children}) => {
-  return(
-    <Content
-      className="site-layout"
-      style={{
-        padding: '0 50px',
-        background: "white"
-      }}
-    >
-      <div
-        style={{
-          padding: 24,
-          minHeight: 380,
-          backgroundColor:'#f9f9f9'
-        }}
-      >
-        <div className='flex justify-around'>
-          <NavGen navLinks={navLinks}/>
-        </div>
-      </div>
-      {children}
-    </Content>
-  )
-}
+	{
+		name: "Пицца",
+		icon: pizzaIcon,
+	},
+	{
+		name: "Суши",
+		icon: sushiIcon,
+	},
+	{
+		name: "Напитки",
+		icon: drinkIcon,
+	},
+	{
+		name: "Снэки",
+		icon: snackIcon,
+	},
+	{
+		name: "Комбо",
+		icon: comboIcon,
+	},
+	{
+		name: "Десерты",
+		icon: dessertIcon,
+	},
+	{
+		name: "Соусы",
+		icon: sauceIcon,
+	},
+];
+export const BodyComponent = ({ children }) => {
+	const items = new Array(10).fill({
+		media: pizzaMedia,
+		name: "pizza1",
+		ingredients:
+      "lodcdslcmdj njknjdsknckdjs cjdskcndsjkcndskj cjdskncdsjkcndskj cjdsncsjdkc dksj cjdsncjsdnc lkjsd n",
+		price: 907,
+	});
+	return (
+		<Content
+			className="site-layout"
+			style={{
+				padding: "0 50px",
+				background: "white",
+			}}
+		>
+			<div
+				style={{
+					padding: 24,
+					minHeight: 380,
+					backgroundColor: "#f9f9f9",
+				}}
+			>
+				<div className="flex justify-between pb-8 mx-auto max-w-[1200px] my-0">
+					<NavGen navLinks={navLinks} />
+				</div>
+				<div className="grid grid-cols-4 gap-8 max-w-[1200px] mx-auto my-0">
+					{items.map((item) => (
+						<Product {...item} />
+					))}
+				</div>
+			</div>
+			{children}
+		</Content>
+	);
+};
