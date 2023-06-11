5268309c6a1485a2bd6e4fdc64672178284971eb
DmitriyM <mironowd697@gmail.com>
2023-06-11 23:47:35 +0600

    add autorization form

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import {Button, Layout} from "antd";
-import {ShoppingCartOutlined, UserOutlined} from "@ant-design/icons";
-import {useScrollDirection} from "../handlers/ScrollHandler"
+import AutorizationButton from "./AutorizationButton";
+import {ShoppingCartOutlined} from "@ant-design/icons";
+import {useScrollDirection} from "../handlers/ScrollHandler";
 import CitiesSelect from "../CitiesSelect";
 
 export const HeaderComponent = () => {
-  const scrollDirection = useScrollDirection();
-  return(
-    <div className={`bg-white sticky top-0 h-20 z-20 w-full ${ scrollDirection === "down" ? "-top-20" : "top-0"}`}>
-        <div>
-          <div className="mx-16 h-8 flex flex-row items-center flex-nowrap">
-              <CitiesSelect />
-              <p className='ml-5'>Среднее время доставки:  45 минут</p>
-          </div>
-          <div className="border-b-2 w-full mb-2"/>
-          <div>
-            <div className="mx-20 flex flex-row items-center flex-nowrap">
-              <p className='text-black font-bold grow'>Hexlet Pizza</p>
-              <Button className="bg-orange-400 text-gray-50"><ShoppingCartOutlined></ShoppingCartOutlined>Корзина</Button>
-              <Button className='bg-orange-400 text-gray-50 ml-3'><UserOutlined></UserOutlined></Button>
-            </div>
-          </div>
-        </div>
-    </div>
-  )
-}
+	const scrollDirection = useScrollDirection();
+	return(
+		<div className={`bg-white sticky top-0 h-20 z-20 w-full ${ scrollDirection === "down" ? "-top-20" : "top-0"}`}>
+			<div>
+				<div className="mx-16 h-8 flex flex-row items-center flex-nowrap">
+					<CitiesSelect />
+					<p className='ml-5'>Среднее время доставки:  45 минут</p>
+				</div>
+				<div className="border-b-2 w-full mb-2"/>
+				<div>
+					<div className="mx-20 flex flex-row items-center flex-nowrap">
+						<p className='text-black font-bold grow'>Hexlet Pizza</p>
+						<Button className="bg-orange-400 text-gray-50"><ShoppingCartOutlined></ShoppingCartOutlined>Корзина</Button>
+						<AutorizationButton></AutorizationButton>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/store/items.js
+++ src/store/items.js
@@ -1,7 +1,7 @@
 import { createEffect, createStore } from "effector";
 import axios from "axios";
 export const getItems = createEffect(()=>{
-	return axios.get("http://localhost:3000/items").then((e)=>{
+	return axios.get("http://localhost:3001/items").then((e)=>{
 		return e.data;
 	});
 });
