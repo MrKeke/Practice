1dbd81655f6e3de5cc543313b5aa042b40d0bf20
MrKeke <alexgraf0701@gmail.com>
2023-06-16 22:49:01 +0300

    add ref

--- src/components/LayoutComponents/Body.js
+++ src/components/LayoutComponents/Body.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { NavGen } from "../NavGen";
 import { Layout } from "antd";
 import { Product } from "../product/Product";
@@ -6,38 +6,55 @@ const { Content } = Layout;
 import {getData} from "../../utils/requests";
 
 
+
+
+export const BodyComponent = () => {
+
+const pizzaRef = useRef(null)
+const sushiRef = useRef(null)
+const drinksRef = useRef(null)
+const comboRef = useRef(null)
+const snackRef = useRef(null)
+const dessertRef = useRef(null)
+const sauceRef = useRef(null)
+
 const navLinks = [
 	{
 		name: "Пиццы",
 		icon: '/nav/pizza.jpg',
+		ref:pizzaRef
 	},
 	{
 		name: "Суши",
 		icon: '/nav/sushi.jpg',
+		ref:sushiRef
 	},
 	{
 		name: "Напитки",
 		icon: "/nav/drink.jpg",
+		ref:drinksRef
 	},
 	{
 		name: "Снэки",
 		icon: "/nav/snack.jpg",
+		ref:snackRef
 	},
 	{
 		name: "Комбо",
 		icon: "/nav/combo.jpg",
+		ref:comboRef
 	},
 	{
 		name: "Десерты",
 		icon: "/nav/dessert.jpg",
+		ref:dessertRef
 	},
 	{
 		name: "Соусы",
 		icon: "/nav/sauce.jpg",
+		ref:sauceRef
 	},
 ];
-
-export const BodyComponent = () => {
 
 	const [pizzas, setPizzasItem] = useState([]);
 	const [sushi, setSushiItem] = useState([])
@@ -78,49 +95,49 @@ export const BodyComponent = () => {
 					</div>
 
 
-					<div className="mx-auto max-w-[1200px]" >
+					<div ref={pizzaRef} className="mx-auto max-w-[1200px]" >
 						<h1 className="pb-6 text-3xl font-bold font-dosis">Пиццы</h1>
 						<div className="grid grid-cols-4 gap-8">
 							{pizzas.map(el => <Product {...el} />)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px]">
+					<div ref={sushiRef} className="mx-auto max-w-[1200px]">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Суши</h1>
 						<div className="grid grid-cols-4 gap-8">
 							{sushi.map(el => <Product {...el}/>)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px] ">
+					<div ref={drinksRef} className="mx-auto max-w-[1200px] ">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Напитки</h1>
 						<div className="grid grid-cols-4 gap-8 text-start">
 							{drinks.map(el => <Product {...el}/>)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px] ">
+					<div ref={snackRef} className="mx-auto max-w-[1200px] ">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Снэки</h1>
 						<div className="grid grid-cols-4 gap-8 text-start">
 							{snacks.map(el => <Product {...el}/>)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px] ">
+					<div ref={comboRef} className="mx-auto max-w-[1200px] ">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Комбо</h1>
 						<div className="grid grid-cols-4 gap-8 text-start">
 							{combo.map(el => <Product {...el}/>)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px] ">
+					<div ref={dessertRef} className="mx-auto max-w-[1200px] ">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Десерты</h1>
 						<div className="grid grid-cols-4 gap-8 text-start">
 							{desserts.map(el => <Product {...el}/>)}
 						</div>
 					</div>
 
-					<div className="mx-auto max-w-[1200px] ">
+					<div ref={sauceRef} className="mx-auto max-w-[1200px] ">
 						<h1 className="pb-6 mt-10 text-3xl font-bold font-dosis">Соусы</h1>
 						<div className="grid grid-cols-4 gap-8 text-start">
 							{sauces.map(el => <Product {...el}/>)}
--- src/components/NavGen.jsx
+++ src/components/NavGen.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import _ from 'lodash'
 
 export const NavGen = ({navLinks}) => {
-
+  
   return(
     <>
-    {navLinks.map(({name, icon}) => {
+    {navLinks.map(({name, icon,ref}) => {
+      console.log(ref)
+      const handleClick = () => {
+        ref.current.scrollIntoView({ behavior: 'smooth' });
+      }
         const id = _.uniqueId()
         return(
           <React.Fragment key={id}>
-            <div className='bg-white w-32 h-24 text-center rounded-xl border-[1px] hover:cursor-pointer'>
+            <div onClick={handleClick}  className='bg-white w-32 h-24 text-center rounded-xl border-[1px] hover:cursor-pointer'>
               <img src={icon} alt="#" className='mx-auto mt-4 h-10 w-10 '/>
               <p className='font-bold'>{name}</p>
             </div>
