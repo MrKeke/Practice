bf096591d208b5bac8c3185b83acac98532091a1
MrKeke <alexgraf0701@gmail.com>
2023-06-19 12:25:37 +0300

    change

--- src/index.jsx
+++ src/index.jsx
@@ -13,5 +13,4 @@ root.render(
 	</>
 );
 
-// https://www.figma.com/file/5Up611L8QTVThfzfi9fwI8/%D0%9A%D1%83%D0%B4%D0%B0-%D0%9F%D0%B8%D1%86%D1%86%D0%B0-(Copy)-(Copy)?type=design&t=jOMbmStDRmNqJjxE-6
 reportWebVitals();
