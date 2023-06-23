78ea442d1c5a956a5d66577e8a4188b279e02985
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-06-23 13:49:53 +0300

    issure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Layout } from 'antd';
-import {HeaderComponent} from "./LayoutComponents/Header";
-import {BodyComponent} from "./LayoutComponents/Body";
+import React from "react";
+import { Layout } from "antd";
+import { HeaderComponent } from "./LayoutComponents/Header";
+import { BodyComponent } from "./LayoutComponents/Body";
 import Footer from "./LayoutComponents/Footer";
 
-
-
-export function LayoutComponent(){
-  return (
-    <Layout>
-      <HeaderComponent />
-      <BodyComponent />
-      <Footer trademark={`© Copyright ${new Date().getFullYear()} — Hexlet Pizza`}/>
-    </Layout>
-  );
-};
+export function LayoutComponent() {
+	return (
+		<Layout>
+			<HeaderComponent />
+			<BodyComponent />
+			<div style={{background:"#fff"}}>
+				<Footer
+					trademark={`© Copyright ${new Date().getFullYear()} — Hexlet Pizza`}
+				/>
+			</div>
+		</Layout>
+	);
+}
